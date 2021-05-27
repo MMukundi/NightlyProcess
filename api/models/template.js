47040ce43c05318bcbbda39cd0bd852034cdc58a
Marcel Mukundi <marcelmukundi1@gmail.com>
2021-05-27 12:30:22 -0700

    Mass pruning and renaming

--- api/models/template.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const schema = new Schema({
-	
-})
-
-module.exports = mongoose.model('schema',schema)
---
+++ productionApps/processTicks.js
@@ -0,0 +1,11 @@
+import { parentPort } from 'worker_threads'
+import { getTicks } from '../functions/getTicks.js'
+import mongoose from "mongoose"
+import config from "../config.js"
+
+parentPort.on('message', async (eodInfo) => {
+    await mongoose.connect(config.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(async (e) => {
+        const data = await getTicks(eodInfo.ticker, eodInfo.date)
+        parentPort.postMessage(data)
+    })
+});
